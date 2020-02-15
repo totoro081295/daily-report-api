01ebe2306647b4fb26b49da5294f0f915a797ecd
totoro081295 <totoro081295@gmail.com>
2020-02-15 22:49:51 +0900

    :sparkles: dailyContentsテーブル作成

---
+++ dailycontent/daily_content.go
@@ -0,0 +1,19 @@
+package dailycontent
+
+import (
+	"time"
+
+	"github.com/gofrs/uuid"
+)
+
+// DailyContent struct daily contents
+type DailyContent struct {
+	ID         uuid.UUID  `json:"id" gorm:"primary_key" sql:"type:uuid" name:"id"`
+	TargetDate time.Time  `json:"targetDate" name:"対象日"`
+	Text       string     `json:"text" name:"内容"`
+	CreatedBy  uuid.UUID  `json:"createdBy" name:"作成者" sql:"type:uuid"`
+	UpdatedBy  uuid.UUID  `json:"updatedBy" name:"更新者" sql:"type:uuid"`
+	CreatedAt  time.Time  `json:"createdAt" name:"作成日"`
+	UpdatedAt  time.Time  `json:"updatedAt" name:"更新日"`
+	DeletedAt  *time.Time `json:"deletedAt" name:"削除日"`
+}
