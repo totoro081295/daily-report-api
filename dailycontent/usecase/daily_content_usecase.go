3f340a3b9811cf08a35388a52d7ffc639417efa2
totoro081295 <totoro081295@gmail.com>
2020-02-16 20:41:41 +0900

    :sparkles: dailtContent取得のusecase作成

---
+++ dailycontent/usecase/daily_content_usecase.go
@@ -0,0 +1,37 @@
+package usecase
+
+import (
+	"time"
+
+	"github.com/totoro081295/daily-report-api/dailycontent"
+
+	"github.com/totoro081295/daily-report-api/dailycontent/repository"
+)
+
+type dailyContentUsecase struct {
+	dailyContentRepo repository.DailyContentRepository
+}
+
+// NewDailyContentUsecase mount dailyContent usecase
+func NewDailyContentUsecase(dailyContet repository.DailyContentRepository) DailyContentUsecase {
+	return &dailyContentUsecase{
+		dailyContentRepo: dailyContet,
+	}
+}
+
+// DailyContentUsecase usecase interface
+type DailyContentUsecase interface {
+	GetByTargetDate(date time.Time) (*dailycontent.Response, error)
+}
+
+func (d *dailyContentUsecase) GetByTargetDate(date time.Time) (*dailycontent.Response, error) {
+	dailyContent, err := d.dailyContentRepo.GetByTargetDate(date)
+	if err != nil {
+		return nil, err
+	}
+	res := dailycontent.Response{
+		ID:   dailyContent.ID,
+		Text: dailyContent.Text,
+	}
+	return &res, nil
+}
