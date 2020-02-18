ab521ad4ebcb1b16f60dc7e4668e7741f1588f3a
totoro081295 <totoro081295@gmail.com>
2020-02-18 22:19:02 +0900

    :sparkles: categoryテーブル作成

--- db/migrations/migrations.go
+++ db/migrations/migrations.go
@@ -3,6 +3,7 @@ package migrations
 import (
 	"github.com/jinzhu/gorm"
 	"github.com/totoro081295/daily-report-api/account"
+	"github.com/totoro081295/daily-report-api/category"
 	"github.com/totoro081295/daily-report-api/dailycontent"
 	"github.com/totoro081295/daily-report-api/db"
 	"github.com/totoro081295/daily-report-api/project"
@@ -20,6 +21,7 @@ func Execute() {
 func Migrate(database *gorm.DB) {
 	database.AutoMigrate(
 		&account.Account{},
+		&category.Category{},
 		&dailycontent.DailyContent{},
 		&refreshtoken.RefreshToken{},
 		&project.Project{},
@@ -30,6 +32,7 @@ func Migrate(database *gorm.DB) {
 func DropTable(database *gorm.DB) {
 	database.DropTableIfExists(
 		&account.Account{},
+		&category.Category{},
 		&dailycontent.DailyContent{},
 		&refreshtoken.RefreshToken{},
 		&project.Project{},
