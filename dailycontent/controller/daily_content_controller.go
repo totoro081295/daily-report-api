2a783ad5af8995ab6d185c597bb2c2b0d7dc4a6b
totoro081295 <totoro081295@gmail.com>
2020-02-18 12:59:46 +0900

    :sparkles: dailyContent作成のcontroller作成

--- dailycontent/controller/daily_content_controller.go
+++ dailycontent/controller/daily_content_controller.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/totoro081295/daily-report-api/dailycontent"
+
 	"github.com/labstack/echo"
 	"github.com/totoro081295/daily-report-api/dailycontent/usecase"
 	"github.com/totoro081295/daily-report-api/middleware"
@@ -21,6 +23,7 @@ func NewDailyContentController(e *echo.Echo, dailyContent usecase.DailyContentUs
 		usecase: dailyContent,
 	}
 	e.GET("/daily-contents/:date", handler.GetByTargetDate, jwt.JWT())
+	e.POST("/daily-contents", handler.Create, jwt.JWT())
 }
 
 // GetByTargetDate 対象日のdailyContentを取得する
@@ -35,4 +38,19 @@ func (c *DailyContentController) GetByTargetDate(ctx echo.Context) error {
 		return status.ResponseError(ctx, err)
 	}
 	return ctx.JSON(http.StatusOK, res)
+}
+
+// Create dailyContentを作成する
+func (c *DailyContentController) Create(ctx echo.Context) error {
+	request := dailycontent.CreatePayload{}
+	err := ctx.Bind(&request)
+	if err != nil {
+		return ctx.JSON(http.StatusUnprocessableEntity, err.Error())
+	}
+
+	res, err := c.usecase.Create(&request)
+	if err != nil {
+		return status.ResponseError(ctx, err)
+	}
+	return ctx.JSON(http.StatusCreated, res)
 }
